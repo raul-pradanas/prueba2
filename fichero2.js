0f247e8ad88d85f9d1bb37a6a64fbd7ae9d40adc
raulpradanas <raulpradanas@devanddel.com>
2022-02-15 17:03:38 +0100

    He añadido un nuevo fichero

---
+++ fichero2.js
@@ -0,0 +1,47 @@
+console.log (ordenar([5,4,3,2,1,0]));
+
+function ordenar(numeros){
+
+   /* Extraemos los impares de la lista */
+
+   let impares = [];
+
+   numeros.forEach(function(numero) {
+
+     if ((numero % 2) == 1) {
+
+       impares.push(numero);
+
+     }
+
+   });
+
+   /* Ordenamos los números impares*/
+
+   impares = impares.sort();
+
+   /* Recorremos el array reemplazado los números
+
+      impares por el impar ordenado que corresponda,
+
+      dejando los pares en su misma posición*/
+
+   let salida = [];
+
+   numeros.forEach(function(numero) {
+
+     if ((numero % 2) == 1) {
+
+       salida.push(impares.shift());
+
+     } else {
+
+       salida.push(numero);
+
+     }
+
+   }); 
+
+   return salida;
+
+}
